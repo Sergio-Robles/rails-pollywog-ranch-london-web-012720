d6123470c0a18ad5659afd84495f35ba3b6a57ef
Sergio Robles <sroblesc@gmail.com>
2020-05-01 15:37:18 +0100

    Saved

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922151507) do
-
-  create_table "frogs", force: :cascade do |t|
-    t.string  "name"
-    t.string  "color"
-    t.integer "pond_id"
-  end
-
-  create_table "ponds", force: :cascade do |t|
-    t.string "water_type"
-    t.string "name"
-  end
-
-  create_table "tadpoles", force: :cascade do |t|
-    t.string  "name"
-    t.string  "color"
-    t.integer "frog_id"
-  end
+ActiveRecord::Schema.define(version: 20140922151423) do
 
 end
